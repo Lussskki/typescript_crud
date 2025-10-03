579e15b286e00c399f25f2fa61ff7c7a7cb90a6f
Luka <lussskki@gmail.com>
2025-10-03 15:36:47 +0400

    in tsconfig added 'allowImportingTsExtensions: true' and 'noEmit:true', in index.ts is runing server on port 3000

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import express from "express"
+import express, { type Application } from "express"
+import userRouter from "./routes/userRouter.ts"
 
-const app = express()
 
-app.get('/ts', (req,res) => {
-    res.send('Hello from ts')
-})
+const app : Application = express()
+
+app.use(express.json())
+app.use('/api', userRouter)
 
 app.listen(3000, () => {
     console.log('running 3000')
