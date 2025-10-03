56dfdee2080c408b7f553244d3005ffaa2c18c77
Luka <lussskki@gmail.com>
2025-10-03 16:24:50 +0400

    added to the string to eliminate confusion

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -10,10 +10,10 @@ import {
 const userRouter = Router()
 
 // CRUD routes
-userRouter.get("/", getUsers)       // GET all users
-userRouter.get("/:id", getUser)     // GET single user by ID
-userRouter.post("/", createUser)    // POST create new user
-userRouter.put("/:id", updateUser)  // PUT update user
-userRouter.delete("/:id", deleteUser) // DELETE user
+userRouter.get("/get", getUsers)       // GET all users
+userRouter.get("/single/:id", getUser)     // GET single user by ID
+userRouter.post("/post", createUser)    // POST create new user
+userRouter.put("/put/:id", updateUser)  // PUT update user
+userRouter.delete("/delete/:id", deleteUser) // DELETE user
 
 export default userRouter
