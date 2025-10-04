8967c4caa18a2e5b558e00e1e29dffa3eac507fa
Luka <lussskki@gmail.com>
2025-10-04 17:38:38 +0400

    add signup and mongodb connect

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { type Application } from "express"
 // import userRouter from "./routes/userRouter.ts"
+import singupRouter from "./routes/singupRouter.ts"
 
+import { connectDB } from "./database/mongoose.ts"
+connectDB()
 
 const app : Application = express()
 
 app.use(express.json())
 // app.use('/api', userRouter)
+app.use('/api', singupRouter)
 
 app.listen(3000, () => {
     console.log('running 3000')
