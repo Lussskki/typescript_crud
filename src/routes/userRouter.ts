234f246d11dc09c2ede7badee6fdfd058552b2d0
Luka <lussskki@gmail.com>
2025-10-04 17:39:41 +0400

    hide the code

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,19 +1,19 @@
-import { Router } from 'express'
-import {
-  getUsers,
-  getUser,
-  createUser,
-  updateUser,
-  deleteUser,
-} from "../controllers/userController.ts"
+// import { Router } from 'express'
+// import {
+//   getUsers,
+//   getUser,
+//   createUser,
+//   updateUser,
+//   deleteUser,
+// } from "../controllers/userController.ts"
 
-const userRouter = Router()
+// const userRouter = Router()
 
-// CRUD routes
-userRouter.get("/get", getUsers)       // GET all users
-userRouter.get("/single/:id", getUser)     // GET single user by ID
-userRouter.post("/post", createUser)    // POST create new user
-userRouter.put("/put/:id", updateUser)  // PUT update user
-userRouter.delete("/delete/:id", deleteUser) // DELETE user
+// // CRUD routes
+// userRouter.get("/get", getUsers)       // GET all users
+// userRouter.get("/single/:id", getUser)     // GET single user by ID
+// userRouter.post("/post", createUser)    // POST create new user
+// userRouter.put("/put/:id", updateUser)  // PUT update user
+// userRouter.delete("/delete/:id", deleteUser) // DELETE user
 
-export default userRouter
+// export default userRouter
