860f68d20c09e1ca3e7af0d12a2d39781a2d069f
Luka <lussskki@gmail.com>
2025-10-04 15:15:55 +0400

    hide this code because dont need crud yet

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express, { type Application } from "express"
-import userRouter from "./routes/userRouter.ts"
+// import userRouter from "./routes/userRouter.ts"
 
 
 const app : Application = express()
 
 app.use(express.json())
-app.use('/api', userRouter)
+// app.use('/api', userRouter)
 
 app.listen(3000, () => {
     console.log('running 3000')
