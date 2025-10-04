c10bb49265a30c0ea869e5dd685af40e0db89745
Luka <lussskki@gmail.com>
2025-10-04 19:00:10 +0400

    add login

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { type Application } from "express"
 // import userRouter from "./routes/userRouter.ts"
 import singupRouter from "./routes/singupRouter.ts"
+import loginRouter from "./routes/loginRouter.ts"
 
 import { connectDB } from "./database/mongoose.ts"
 connectDB()
@@ -10,6 +11,7 @@ const app : Application = express()
 app.use(express.json())
 // app.use('/api', userRouter)
 app.use('/api', singupRouter)
+app.use('/api', loginRouter)
 
 app.listen(3000, () => {
     console.log('running 3000')
